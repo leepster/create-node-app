82bae652e8b5b53b1a833d5ca7b1c44f9eebfcad
Tony Leeper <tonyleeper@blueyonder.co.uk>
2022-02-12 19:28:14 +0000

    added shell commands for git init and npm init

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk'
 import { readdir, cp } from 'fs/promises'
 import { join, dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
-import chalk from 'chalk'
+import { exec } from 'child_process'
 
 async function* readdirRecursive(path) {
   const entries = await readdir(path, { withFileTypes: true })
@@ -17,7 +18,27 @@ async function* readdirRecursive(path) {
   }
 }
 
+async function execShellCommand(command) {
+  return new Promise((resolve) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(chalk.redBright(error.message))
+      }
+
+      if (stderr) {
+        console.log(chalk.redBright(stderr))
+      }
+
+      console.log(chalk.greenBright(stdout))
+      resolve()
+    })
+  })
+}
+
 async function main() {
+  await execShellCommand('git init')
+  await execShellCommand('npm init -y')
+
   const source = join(dirname(fileURLToPath(import.meta.url)), './template')
   await cp(source, process.cwd(), { recursive: true })
 
