d4a9ee7e7c36d1827ff8f907323f861b82a9dc68
Tony Leeper <tonyleeper@blueyonder.co.uk>
2022-02-13 18:43:38 +0000

    adding correct package-name to LICENSE file

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 import chalk from 'chalk'
 import { existsSync } from 'fs'
-import { readdir, cp, writeFile, mkdir } from 'fs/promises'
-import { join, dirname, resolve } from 'path'
+import { readdir, cp, writeFile, mkdir, readFile } from 'fs/promises'
+import { join, dirname, resolve, basename } from 'path'
 import { fileURLToPath } from 'url'
 import { exec } from 'child_process'
 
@@ -38,11 +38,26 @@ async function execShellCommand(command) {
   })
 }
 
-async function main() {
-  await execShellCommand('git init')
-  await execShellCommand('npm init -y')
-  await execShellCommand('npm i prettier -D')
+async function createIdeaPrettierSettingsFile() {
+  if (!existsSync(join(process.cwd(), '.idea'))) {
+    await mkdir(join(process.cwd(), '.idea'))
+  }
 
+  await writeFile(
+    join(process.cwd(), '.idea/prettier.xml'),
+    `<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="PrettierConfiguration">
+    <option name="myRunOnSave" value="true" />
+    <option name="myRunOnReformat" value="true" />
+  </component>
+</project>`
+  )
+
+  console.log(chalk.greenBright('created:'), chalk.cyanBright('.idea/prettier.xml'))
+}
+
+async function copyTemplateFiles() {
   const source = join(dirname(fileURLToPath(import.meta.url)), './template')
 
   // TODO: why isn't this copying the .gitignore file and the .idea folder? - applied a manual workaround instead below
@@ -54,29 +69,37 @@ async function main() {
       chalk.cyanBright(filename.replace(join(dirname(fileURLToPath(import.meta.url)), 'template/'), ''))
     )
   }
+}
 
+async function createGitIgnoreFile() {
   await writeFile(
     join(process.cwd(), '.gitignore'),
     `.idea
 node_modules`
   )
   console.log(chalk.greenBright('created:'), chalk.cyanBright('.gitignore'))
+}
 
-  if (!existsSync(join(process.cwd(), '.idea'))) {
-    await mkdir(join(process.cwd(), '.idea'))
-  }
-  await writeFile(
-    join(process.cwd(), '.idea/prettier.xml'),
-    `<?xml version="1.0" encoding="UTF-8"?>
-<project version="4">
-  <component name="PrettierConfiguration">
-    <option name="myRunOnSave" value="true" />
-    <option name="myRunOnReformat" value="true" />
-  </component>
-</project>`
-  )
-  console.log(chalk.greenBright('created:'), chalk.cyanBright('.idea/prettier.xml'))
+async function replaceTextInFile(filepath, text, replacement) {
+  const contents = await readFile(filepath, { encoding: 'utf-8' })
+  const regex = new RegExp(text, 'g')
+  await writeFile(filepath, contents.replace(regex, replacement))
+}
 
+async function getPackageName() {
+  const contents = await readFile('./package.json', { encoding: 'utf-8' })
+  const settings = JSON.parse(contents)
+  return settings.name
+}
+
+async function main() {
+  await execShellCommand('git init')
+  await execShellCommand('npm init -y')
+  await execShellCommand('npm i prettier -D')
+  await copyTemplateFiles()
+  await replaceTextInFile('./LICENSE', '{{package-name}}', await getPackageName())
+  await createGitIgnoreFile()
+  await createIdeaPrettierSettingsFile()
   await execShellCommand('git add .')
   await execShellCommand('git commit -m "initial commit"')
 }
